89b85f7b16a42d8c7a55d6d36776d7d3b304f017
captain-blue210 <captain.blue210@gmail.com>
2020-10-11 21:43:06 +0900

    study access modifier.

--- class-section.js
+++ class-section.js
@@ -1,12 +1,18 @@
 "use strict";
 var Person = /** @class */ (function () {
-    function Person(initName) {
+    function Person(initName, initAge) {
         this.name = initName;
+        this.age = initAge;
     }
     Person.prototype.greeting = function () {
-        console.log("Hello! My name is " + this.name);
+        console.log("Hello! My name is " + this.name + ". I'm " + this.age + " years old.");
+    };
+    Person.prototype.incrementAge = function () {
+        this.age += 1;
     };
     return Person;
 }());
-var steve = new Person('Steve');
+var steve = new Person('Steve', 32);
+steve.greeting();
+steve.incrementAge();
 steve.greeting();
--- class-section.ts
+++ class-section.ts
@@ -1,14 +1,23 @@
 class Person {
+    // default is public
     name: string
+    private age: number
 
-    constructor(initName: string) {
+    constructor(initName: string, initAge: number) {
         this.name = initName;
+        this.age = initAge;
     }
 
-    greeting(this: {name: string}) {
-        console.log(`Hello! My name is ${this.name}`);
+    greeting(this: Person) {
+        console.log(`Hello! My name is ${this.name}. I'm ${this.age} years old.`);
+    }
+
+    incrementAge() {
+        this.age += 1;
     }
 }
 
-const steve = new Person('Steve');
+const steve = new Person('Steve', 32);
+steve.greeting();
+steve.incrementAge();
 steve.greeting();
