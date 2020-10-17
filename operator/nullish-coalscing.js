4b3b0de8084a99abded782a51e7b8be885af98f0
captain-blue210 <captain.blue210@gmail.com>
2020-10-17 13:54:18 +0900

    learn nullish-coalescing and Optional Chaining.

--- operator/nullish-coalscing.js
+++ operator/nullish-coalscing.js
@@ -15,6 +15,15 @@ const users = [
 
 for (u of users) {
   const user = u ?? { name: "(Somebody)" };
+  /**
+   * nullish-coalescing -> ??
+   * 左辺がnullまたはundefineのときだけ右辺が評価される
+   * OR演算子でも似たようなことができるが、orの場合は0や空文字もスルーされてしまうので、
+   * より厳密なこちらを使うほうがよい
+   *
+   * Optional Chaining -> ?.
+   * 指定したキーのプロパティ・メソッドが存在しない場合、undefinedを返す（TypeErrorにならず、if文での判定を減らせる）
+   **/
   const town = user?.address?.town ?? "(Somewhere)";
   console.log(`${user.name} lives in ${town}`);
 }
