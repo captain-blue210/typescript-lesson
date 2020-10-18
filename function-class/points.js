00e3f2c954ad73ed552797686a0bf9ada5b3f9fc
captain-blue210 <captain.blue210@gmail.com>
2020-10-18 16:40:30 +0900

    learn TypeScript class.

---
+++ function-class/points.js
@@ -0,0 +1,10 @@
+var Point = /** @class */ (function () {
+    function Point() {
+        this.x = 0;
+        this.y = 0;
+    }
+    return Point;
+}());
+var pointA = new Point();
+var pointB = { x: 2, y: 4 };
+var pointC = { x: 5, y: 5, z: 10 };
