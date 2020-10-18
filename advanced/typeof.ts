ddee2590f1d5a51e5bcd3e30a1b6d2e1cac79890
captain-blue210 <captain.blue210@gmail.com>
2020-10-18 17:33:50 +0900

    learn typeof

---
+++ advanced/typeof.ts
@@ -0,0 +1,10 @@
+// typeofで変数から型を抽出する
+console.log(typeof 100); // 'number'
+
+const arr = [1, 2, 3];
+console.log(typeof arr); // 'object'
+
+type NumArr = typeof arr;
+
+const val: NumArr = [4, 5, 6];
+// const val2: NumArr = ['foo', 'bar', 'baz'];  /* compile error */
